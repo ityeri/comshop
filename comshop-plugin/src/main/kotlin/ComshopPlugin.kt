1093b44fafcc34cfb2fe3c3d8590a05ba3a56c76
ityeri <ityeri9@gmail.com>
2025-11-09 15:11:22 +0900

    Add example code

--- comshop-plugin/src/main/kotlin/ComshopPlugin.kt
+++ comshop-plugin/src/main/kotlin/ComshopPlugin.kt
@@ -61,6 +61,8 @@ class ComshopPlugin : JavaPlugin() {
         }
 
         CommandRegistrar.register(command)
+
+        CommandRegistrar.register(ExampleCommand())
     }
 
     override fun onDisable() {
