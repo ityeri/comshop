dda694e0d16a56abf8e29cb24e034661187d10b8
ityeri <ityeri9@gmail.com>
2025-11-09 15:10:59 +0900

    Add LiteralCommandBuilder

--- comshop-core/src/main/kotlin/com/github/ityeri/comshop/builder/CommandDSL.kt
+++ comshop-core/src/main/kotlin/com/github/ityeri/comshop/builder/CommandDSL.kt
@@ -8,80 +8,12 @@ import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
 import io.papermc.paper.command.brigadier.CommandSourceStack
 
-class CommandDSL(val name: String) : CommandBuilder {
-    private val arguments: MutableList<ArgumentData<*>> = mutableListOf()
-    private val subCommands: MutableList<CommandBuilder> = mutableListOf()
-    private var executor: ContextWrapper<CommandSourceStack>.(CommandSourceStack) -> Int =
-        { source -> 1 }
-    private var permissionChecker: (source: CommandSourceStack) -> Boolean = { true }
-
-
+class CommandDSL(override val name: String) : LiteralCommandBuilder() {
     companion object {
         fun command(name: String, block: CommandDSL.() -> Unit): CommandBuilder {
             val commandDsl = CommandDSL(name)
             commandDsl.apply(block)
             return commandDsl
         }
-    }
-
-
-    fun requires(block: (source: CommandSourceStack) -> Boolean) {
-        permissionChecker = block
-    }
-
-    fun arguments(block: ArgumentRegistrar.() -> Unit) {
-        ArgumentRegistrar(arguments).apply(block)
-    }
-
-    fun executes(block: ContextWrapper<CommandSourceStack>.(source: CommandSourceStack) -> Int) {
-        executor = block
-    }
-
-    fun then(block: () -> CommandBuilder) {
-        subCommands.add(block())
-    }
-
-    fun then(name: String, block: CommandDSL.() -> Unit) {
-        val commandDsl = CommandDSL(name)
-        subCommands.add(commandDsl)
-        commandDsl.apply(block)
-    }
-
-
-    override fun createBuilder(): LiteralArgumentBuilder<CommandSourceStack> {
-        val rootBuilder = literal<CommandSourceStack>(name)
-
-        rootBuilder.requires { source -> permissionChecker(source) }
-
-        for (subCommand in subCommands) {
-            rootBuilder.then(subCommand.createBuilder())
-        }
-
-        // TODO 아무리 봐도 이 코드는 좀 아닌것 같음 아니다 꽤 괜찮을지도
-        if (arguments.isEmpty()) {
-            rootBuilder.executes { context ->
-                ContextWrapper(context).run { executor(context.source) }
-            }
-
-        } else {
-            var previousBuilder: ArgumentBuilder<CommandSourceStack, *>? = null
-            var currentBuilder: ArgumentBuilder<CommandSourceStack, *>? = null
-
-            for (argumentData in arguments.reversed()) {
-                currentBuilder = argumentData.createBuilder<CommandSourceStack>()
-
-                if (previousBuilder == null) {
-                    previousBuilder = currentBuilder.executes { context ->
-                        ContextWrapper(context).run { executor(context.source) }
-                    }
-                } else {
-                    previousBuilder = currentBuilder.then(previousBuilder)
-                }
-            }
-
-            rootBuilder.then(currentBuilder!!)
-        }
-
-        return rootBuilder
     }
 }
