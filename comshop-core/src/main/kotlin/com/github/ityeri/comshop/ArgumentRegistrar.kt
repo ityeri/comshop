860ca8fb844638b18c22ee08ae46b3b6b2ff4b04
it <ityeri9@gmail.com>
2025-12-11 08:31:34 +0900

    refactor: Rename SingleArgumentData -> SingleArgumentNode

--- comshop-core/src/main/kotlin/com/github/ityeri/comshop/ArgumentRegistrar.kt
+++ comshop-core/src/main/kotlin/com/github/ityeri/comshop/ArgumentRegistrar.kt
@@ -1,10 +1,10 @@
 package com.github.ityeri.comshop
 
-import com.github.ityeri.comshop.argument.SingleArgumentData
+import com.github.ityeri.comshop.argument.SingleArgumentNode
 import com.mojang.brigadier.arguments.ArgumentType
 
-class ArgumentRegistrar(val arguments: MutableList<SingleArgumentData<*>>) {
+class ArgumentRegistrar(val arguments: MutableList<SingleArgumentNode<*>>) {
     operator fun <T> String.invoke(block: () -> ArgumentType<T>) {
-        arguments.add(SingleArgumentData(block(), this))
+        arguments.add(SingleArgumentNode(block(), this))
     }
 }
--- comshop-core/src/main/kotlin/com/github/ityeri/comshop/argument/SingleArgumentNode.kt
+++ comshop-core/src/main/kotlin/com/github/ityeri/comshop/argument/SingleArgumentNode.kt
@@ -4,7 +4,7 @@ import com.mojang.brigadier.arguments.ArgumentType
 import com.mojang.brigadier.builder.ArgumentBuilder
 import com.mojang.brigadier.builder.RequiredArgumentBuilder
 
-class SingleArgumentData<T>(val argumentType: ArgumentType<T>, val name: String): ArgumentNode {
+class SingleArgumentNode<T>(val argumentType: ArgumentType<T>, val name: String): ArgumentNode {
     override fun <S> connectArgumentBuilder(argumentBuilders: List<ArgumentBuilder<S, *>>):
             List<ArgumentBuilder<S, *>> {
 
