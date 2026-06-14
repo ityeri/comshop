9d23f31edf38be35a48132e716b6e37049961698
ityeri <ityeri9@gmail.com>
2026-06-14 20:58:04 +0900

    edit: foojay plugin version changed to 1.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // Use the Foojay Toolchains plugin to automatically download JDKs required by subprojects.
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 dependencyResolutionManagement {
