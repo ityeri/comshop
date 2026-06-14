44c46163d86a346375daf67d6c809caccf7b8538
ityeri <ityeri9@gmail.com>
2026-06-14 15:25:15 +0900

    rm: NativeArgumentTypeConverter and related implementations

--- comshop-impl/1.21.10/src/main/kotlin/com/github/ityeri/comshop/impl/converter/argument/nativ/NativeArgumentTypeConverter.kt
+++
@@ -1,9 +0,0 @@
-package com.github.ityeri.comshop.impl.converter.argument.nativ
-
-import com.github.ityeri.comshop.api.argument.NativeArgumentType
-import com.mojang.brigadier.arguments.ArgumentType
-
-
-abstract class NativeArgumentTypeConverter<T : Any, A : NativeArgumentType<T>>(
-    val converter: (original: A) -> ArgumentType<T>
-)
