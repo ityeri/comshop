010f1fb281148a1de10879f104767e4712b9f192
ityeri <ityeri9@gmail.com>
2026-07-02 14:07:47 +0900

    edit: Changed to make all functions of the ArgumentNodeBuilderFactory returns subtypes of NativeArgumentType directly

--- comshop-front/src/main/kotlin/com/github/ityeri/comshop/builder/ArgumentNodeBuilderFactory.kt
+++ comshop-front/src/main/kotlin/com/github/ityeri/comshop/builder/ArgumentNodeBuilderFactory.kt
@@ -6,76 +6,69 @@ import io.papermc.paper.registry.RegistryKey
 
 
 open class ArgumentNodeBuilderFactory {
-    fun <T : Any> fromNativeType(
-        argumentType: NativeArgumentType<T>
-    ): ArgumentStructureBuilder.SingleArgumentBuilder<T> =
-        ArgumentStructureBuilder.SingleArgumentBuilder(
-            null, argumentType
-        )
-
     // Primitive types
-    fun boolean() = fromNativeType(NativeArgumentType.BooleanArgumentType())
+    fun boolean() = NativeArgumentType.BooleanArgumentType()
 
     fun int(min: Int = Int.MIN_VALUE, max: Int = Int.MAX_VALUE) =
-        fromNativeType(NativeArgumentType.IntArgumentType(min, max))
+        NativeArgumentType.IntArgumentType(min, max)
 
     fun float(min: Float = Float.MIN_VALUE, max: Float = Float.MAX_VALUE) =
-        fromNativeType(NativeArgumentType.FloatArgumentType(min, max))
+        NativeArgumentType.FloatArgumentType(min, max)
 
     fun double(min: Double = Double.MIN_VALUE, max: Double = Double.MAX_VALUE) =
-        fromNativeType(NativeArgumentType.DoubleArgumentType(min, max))
+        NativeArgumentType.DoubleArgumentType(min, max)
 
     fun long(min: Long = Long.MIN_VALUE, max: Long = Long.MAX_VALUE) =
-        fromNativeType(NativeArgumentType.LongArgumentType(min, max))
+        NativeArgumentType.LongArgumentType(min, max)
 
-    fun string(type: StringType) = fromNativeType(NativeArgumentType.StringArgumentType(type))
-    fun word() = fromNativeType(NativeArgumentType.StringArgumentType(StringType.WORD))
-    fun quotedString() = fromNativeType(NativeArgumentType.StringArgumentType(StringType.QUOTED))
-    fun greedyString() = fromNativeType(NativeArgumentType.StringArgumentType(StringType.GREEDY))
+    fun string(type: StringType) = NativeArgumentType.StringArgumentType(type)
+    fun word() = NativeArgumentType.StringArgumentType(StringType.WORD)
+    fun quotedString() = NativeArgumentType.StringArgumentType(StringType.QUOTED)
+    fun greedyString() = NativeArgumentType.StringArgumentType(StringType.GREEDY)
 
     // Paper types from io.papermc.paper.command.brigadier.argument.ArgumentTypes
-    fun entity() = fromNativeType(NativeArgumentType.EntityArgumentType())
-    fun entities() = fromNativeType(NativeArgumentType.EntitiesArgumentType())
-    fun player() = fromNativeType(NativeArgumentType.PlayerArgumentType())
-    fun players() = fromNativeType(NativeArgumentType.PlayersArgumentType())
-    fun playerProfiles() = fromNativeType(NativeArgumentType.PlayerProfilesArgumentType())
+    fun entity() = NativeArgumentType.EntityArgumentType()
+    fun entities() = NativeArgumentType.EntitiesArgumentType()
+    fun player() = NativeArgumentType.PlayerArgumentType()
+    fun players() = NativeArgumentType.PlayersArgumentType()
+    fun playerProfiles() = NativeArgumentType.PlayerProfilesArgumentType()
 
-    fun blockPosition() = fromNativeType(NativeArgumentType.BlockPositionArgumentType())
+    fun blockPosition() = NativeArgumentType.BlockPositionArgumentType()
     fun finePosition(centerIntegers: Boolean = false) =
-        fromNativeType(NativeArgumentType.FinePositionArgumentType(centerIntegers))
-    fun rotation() = fromNativeType(NativeArgumentType.RotationArgumentType())
+        NativeArgumentType.FinePositionArgumentType(centerIntegers)
+    fun rotation() = NativeArgumentType.RotationArgumentType()
 
-    fun blockState() = fromNativeType(NativeArgumentType.BlockStateArgumentType())
-    fun itemStack() = fromNativeType(NativeArgumentType.ItemStackArgumentType())
-    fun itemPredicate() = fromNativeType(NativeArgumentType.ItemPredicateArgumentType())
+    fun blockState() = NativeArgumentType.BlockStateArgumentType()
+    fun itemStack() = NativeArgumentType.ItemStackArgumentType()
+    fun itemPredicate() = NativeArgumentType.ItemPredicateArgumentType()
 
-    fun namedColor() = fromNativeType(NativeArgumentType.NamedColorArgumentType())
-    fun component() = fromNativeType(NativeArgumentType.ComponentArgumentType())
-    fun style() = fromNativeType(NativeArgumentType.StyleArgumentType())
-    fun signedMessage() = fromNativeType(NativeArgumentType.SignedMessageArgumentType())
+    fun namedColor() = NativeArgumentType.NamedColorArgumentType()
+    fun component() = NativeArgumentType.ComponentArgumentType()
+    fun style() = NativeArgumentType.StyleArgumentType()
+    fun signedMessage() = NativeArgumentType.SignedMessageArgumentType()
 
-    fun scoreboardDisplaySlot() = fromNativeType(NativeArgumentType.ScoreboardDisplaySlotArgumentType())
-    fun namespacedKey() = fromNativeType(NativeArgumentType.NamespacedKeyArgumentType())
-    fun key() = fromNativeType(NativeArgumentType.KeyArgumentType())
+    fun scoreboardDisplaySlot() = NativeArgumentType.ScoreboardDisplaySlotArgumentType()
+    fun namespacedKey() = NativeArgumentType.NamespacedKeyArgumentType()
+    fun key() = NativeArgumentType.KeyArgumentType()
 
-    fun integerRange() = fromNativeType(NativeArgumentType.IntegerRangeArgumentType())
-    fun doubleRange() = fromNativeType(NativeArgumentType.DoubleRangeArgumentType())
+    fun integerRange() = NativeArgumentType.IntegerRangeArgumentType()
+    fun doubleRange() = NativeArgumentType.DoubleRangeArgumentType()
 
-    fun world() = fromNativeType(NativeArgumentType.WorldArgumentType())
-    fun gameMode() = fromNativeType(NativeArgumentType.GameModeArgumentType())
-    fun heightMap() = fromNativeType(NativeArgumentType.HeightMapArgumentType())
-    fun uuid() = fromNativeType(NativeArgumentType.UuidArgumentType())
-    fun objectiveCriteria() = fromNativeType(NativeArgumentType.ObjectiveCriteriaArgumentType())
-    fun entityAnchor() = fromNativeType(NativeArgumentType.EntityAnchorArgumentType())
+    fun world() = NativeArgumentType.WorldArgumentType()
+    fun gameMode() = NativeArgumentType.GameModeArgumentType()
+    fun heightMap() = NativeArgumentType.HeightMapArgumentType()
+    fun uuid() = NativeArgumentType.UuidArgumentType()
+    fun objectiveCriteria() = NativeArgumentType.ObjectiveCriteriaArgumentType()
+    fun entityAnchor() = NativeArgumentType.EntityAnchorArgumentType()
 
-    fun time(minTime: Int = 0) = fromNativeType(NativeArgumentType.TimeArgumentType(minTime))
+    fun time(minTime: Int = 0) = NativeArgumentType.TimeArgumentType(minTime)
 
-    fun templateMirror() = fromNativeType(NativeArgumentType.TemplateMirrorArgumentType())
-    fun templateRotation() = fromNativeType(NativeArgumentType.TemplateRotationArgumentType())
+    fun templateMirror() = NativeArgumentType.TemplateMirrorArgumentType()
+    fun templateRotation() = NativeArgumentType.TemplateRotationArgumentType()
 
     fun <T : Any> resource(registryKey: RegistryKey<T>) =
-        fromNativeType(NativeArgumentType.ResourceArgumentType(registryKey))
+        NativeArgumentType.ResourceArgumentType(registryKey)
 
     fun <T : Any> resourceKey(registryKey: RegistryKey<T>) =
-        fromNativeType(NativeArgumentType.ResourceKeyArgumentType(registryKey))
+        NativeArgumentType.ResourceKeyArgumentType(registryKey)
 }
