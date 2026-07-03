8adbccbbbac723faae7ccb6f6f7493f6ce4579b4
ityeri <ityeri9@gmail.com>
2026-07-03 13:53:18 +0900

    add: selectArgument function

--- comshop-front/src/main/kotlin/com/github/ityeri/comshop/Comshop.kt
+++ comshop-front/src/main/kotlin/com/github/ityeri/comshop/Comshop.kt
@@ -1,6 +1,8 @@
 package com.github.ityeri.comshop
 
 import com.github.ityeri.comshop.api.argument.ComshopCustomArgumentType
+import com.github.ityeri.comshop.api.argument.StringType
+import com.github.ityeri.comshop.api.exception.ComshopCommandException
 import com.github.ityeri.comshop.builder.CommandBuilder
 import com.github.ityeri.comshop.builder.CustomArgumentTypeBuilder
 import org.bukkit.plugin.java.JavaPlugin
@@ -27,3 +29,44 @@ fun <T : Any, N : Any> customArgument(
     block: CustomArgumentTypeBuilder<T, N>.() -> Unit
 ): ComshopCustomArgumentType<T, N> =
     CustomArgumentTypeBuilder<T, N>().apply(block).build()
+
+fun selectArgument(
+    vararg element: String,
+    stringType: StringType = StringType.WORD,
+    ignoreCase: Boolean = true,
+    displayOnlyMatches: Boolean = true,
+    whenException: (String) -> String = { userInput ->
+        throw ComshopCommandException("Value \"${userInput}\" does not exist.")
+    }
+) = customArgument {
+    val elements = element.toList()
+
+    native(string(stringType))
+
+    parses { nativeValue, source ->
+        val findValue = (if (ignoreCase) elements.map { it.lowercase() } else elements)
+            .find {
+                (if (ignoreCase) nativeValue.lowercase() else nativeValue) == it
+            }
+
+        findValue ?: whenException.invoke(nativeValue)
+    }
+
+    suggests {
+        val displayingElements = if (stringType == StringType.QUOTED) {
+            elements.map { "\"" + it + "\"" }
+        } else {
+            elements
+        }
+
+        if (displayOnlyMatches) {
+            displayingElements.filter {
+                it.removePrefix("\"").startsWith(context.remining, ignoreCase = true)
+            }
+        } else {
+            displayingElements
+        }.forEach {
+            suggest(it)
+        }
+    }
+}
